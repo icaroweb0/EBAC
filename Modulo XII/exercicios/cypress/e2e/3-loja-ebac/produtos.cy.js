e1937daeeaad99cbafc8ea42712314dbd2bf6aaa
Icaro Siqueira Pinto <icaroweb@hotmail.com>
2024-03-20 11:16:54 -0300

    Aula1: Arquivo de dados

---
+++ Modulo XII/exercicios/cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+    beforeEach(() => {
+        cy.visit('produtos')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('#content')
+            //.first()
+            //.last()
+            //.eq(2)
+            .contains('Aero Daily')
+                .click()
+        
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+            
+    });
+});
